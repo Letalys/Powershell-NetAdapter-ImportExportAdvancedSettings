98535d3c285033d8f94f8d023d4d3966ae3eda85
Letalys <113456828+Letalys@users.noreply.github.com>
2023-02-19 15:08:34 +0100

    Initial Commit for v1 scripts
    Script v1 is using Net-Adapter module

---
+++ v1/NetAdapter-ExportAdvSettings.ps1
@@ -0,0 +1,80 @@
+#Requires -Version 4.0
+
+cls
+Write-Host -ForegroundColor Cyan "------------------------------------------------------------"
+Write-Host -ForegroundColor Cyan "       NetAdapter Advanced Settings Exporting           "
+Write-Host -ForegroundColor Cyan "------------------------------------------------------------"
+Write-Host ""
+
+Write-Host -Foreground Yellow "Physical Network Adapter List :"
+Get-NetAdapter -Physical | Select @{label='Index';expression={$_.IfIndex}},Status,InterfaceDescription,Name, DriverFileName, DriverVersion, DriverDate  | Format-Table -Autosize
+
+$SelectedNetAdapterIndex = Read-Host "Enter the index of the network adapter to export"
+
+Write-host ""
+Write-Host -NoNewLine  -ForegroundColor Yellow "Export..."
+
+$SelectedNetAdapter = Get-NetAdapter -InterfaceIndex $SelectedNetAdapterIndex -ErrorAction SilentlyContinue
+
+if ($SelectedNetAdapter -ne $null){
+    Try {
+        $WMIComputerInfo = Get-WmiObject -Class  Win32_ComputerSystemProduct
+        $WMIOSInfo = Get-WmiObject -Class  Win32_OperatingSystem
+        $SelectedNetAdapterAdvSettings = $SelectedNetAdapter | Get-NetAdapterAdvancedProperty  | Select * | Sort DisplayName
+
+        $ObjExport = New-Object PSObject
+
+        $ObjComputerInfo = New-Object PSObject
+        $ObjComputerInfo | Add-Member -MemberType NoteProperty -Name "ExportDateTime" -Value $(Get-Date -Format "yyyy-MM-dd HH:mm")
+        $ObjComputerInfo | Add-Member -MemberType NoteProperty -Name "ComputerSource" -Value $env:COMPUTERNAME
+        $ObjComputerInfo | Add-Member -MemberType NoteProperty -Name "Manufacturer" -Value $WMIComputerInfo.Vendor
+        $ObjComputerInfo | Add-Member -MemberType NoteProperty -Name "ModelName" -Value $WMIComputerInfo.Name
+        $ObjComputerInfo | Add-Member -MemberType NoteProperty -Name "ModelVersion" -Value $WMIComputerInfo.Version
+        $ObjComputerInfo | Add-Member -MemberType NoteProperty -Name "SerialNumber" -Value $WMIComputerInfo.IdentifyingNumber
+        $ObjComputerInfo | Add-Member -MemberType NoteProperty -Name "OperatingSystemVersion" -Value $WMIOSInfo.Version
+
+        $ObjExport | Add-Member -MemberType NoteProperty -Name "ExportedFrom" -Value $ObjComputerInfo
+
+        $ObjNetAdapterInfo = New-Object PSObject
+        $ObjNetAdapterInfo | Add-Member -MemberType NoteProperty -Name "InterfaceDescription" -Value $SelectedNetAdapter.InterfaceDescription
+        $ObjNetAdapterInfo | Add-Member -MemberType NoteProperty -Name "DriverFileName" -Value $SelectedNetAdapter.DriverFileName
+        $ObjNetAdapterInfo | Add-Member -MemberType NoteProperty -Name "DriverVersion" -Value $SelectedNetAdapter.DriverVersion
+        $ObjNetAdapterInfo | Add-Member -MemberType NoteProperty -Name "DriverDate" -Value $SelectedNetAdapter.DriverDate
+
+        [System.Collections.Arraylist]$SettingsList =@()
+        foreach($Setting in $SelectedNetAdapterAdvSettings){
+            $ObjNetAdapterAdvSettingValueInfo = New-Object PSObject
+            $ObjNetAdapterAdvSettingValueInfo | Add-Member -MemberType NoteProperty -Name "Value" -Value "$($setting.DisplayValue)"
+
+            $SelectNetAdapterValidDisplayAdvSettings = $Setting | Select -ExpandProperty ValidDisplayValues
+
+            [System.Collections.Arraylist]$ValidDisplayValues =@()
+            foreach($ValidDisplayValue in $SelectNetAdapterValidDisplayAdvSettings){
+                $ValidDisplayValues.Add($ValidDisplayValue) | Out-Null
+            }
+
+            $ObjNetAdapterAdvSettingValueInfo | Add-Member -MemberType NoteProperty -Name "ValidDisplayValues" -Value $ValidDisplayValues
+
+            $ObjNetAdapterAdvSetting = New-Object PSObject
+            $ObjNetAdapterAdvSetting | Add-Member -MemberType NoteProperty -Name "$($setting.RegistryKeyWord)" -Value $ObjNetAdapterAdvSettingValueInfo
+
+            $SettingsList.Add($ObjNetAdapterAdvSetting) | Out-Null
+        }
+
+        $ObjNetAdapterInfo | Add-Member -MemberType NoteProperty -Name "AdvancedSettings" -Value $SettingsList
+        $ObjExport | Add-Member -MemberType NoteProperty -Name "NetAdapterInformations" -Value $ObjNetAdapterInfo
+
+        $PathToSave = "$PSScriptRoot\Exports\$($SelectedNetAdapter.InterfaceDescription).NetAdapteExportCfg.xml"
+        $NewXML = ConvertTo-Xml -As "Document" -InputObject $ObjExport -Depth 5 -NoTypeInformation
+        $NewXML.Save($PathToSave)
+
+        Write-Host -ForegroundColor Green "Completed"
+        Write-Host -NoNewLine -ForegroundColor Yellow "Save To : "
+        Write-Host $PathToSave 
+    }Catch{
+        Write-Error $_
+    }
+}else{
+    Write-Error "No NetworkAdapter exists for the specified index." -Category ObjectNotFound
+}
+Write-Host ""
---
+++ v1/NetAdapter-ImportAdvSettings.ps1
@@ -0,0 +1,83 @@
+﻿#Requires -Version 4.0
+Param([String]$ImportXMLFile)
+
+$currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+if($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator) -eq $false){
+    Write-Error "This script requires elevation of privilege to run" -Category AuthenticationError
+    pause
+    exit
+}
+
+cls
+Write-Host -ForegroundColor Cyan "------------------------------------------------------------"
+Write-Host -ForegroundColor Cyan "       NetAdapter Advanced Settings Importing               "
+Write-Host -ForegroundColor Cyan "------------------------------------------------------------"
+Write-Host ""
+
+if($ImportXMLFile -eq ""){
+    Add-Type -AssemblyName System.Windows.Forms
+
+    $FileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{ 
+        Filter = 'XML (*.xml)|*.xml'
+        RestoreDirectory = $true
+        Title ="Choose the configuration file to import"
+        Multiselect =$false
+    }
+    $null = $FileBrowser.ShowDialog()
+    $ImportXMLFile = $FileBrowser.FileName
+}
+
+If((Test-Path $ImportXMLFile) -eq $false){Write-Error "Specified file not exist" -Category NotSpecified;exit}
+
+Try {
+    Write-Host -NoNewLine -ForegroundColor Yellow "Loading File... "
+    $CurrentFile = Get-Item $ImportXMLFile -ErrorAction SilentlyContinue
+    
+    [xml]$XML = Get-Content $CurrentFile.FullName
+    Write-Host -ForegroundColor Green "Completed"
+
+    $NetAdapterIfName = Select-Xml -Xml $XML -XPath "//Property[contains(@Name,'InterfaceDescription')]" | Select -ExpandProperty Node
+    $NetAdapterDriverVers  = Select-Xml -Xml $XML -XPath "//Property[contains(@Name,'DriverVersion')]" | Select -ExpandProperty Node
+    $NetAdapterAdvSettings = Select-Xml -Xml $XML -XPath "//Property[contains(@Name,'AdvancedSettings')]/Property/Property" | Select -ExpandProperty Node
+
+    Write-Host -NoNewline -ForegroundColor Yellow "Checking the presence of the network adapter specified in XML File... "
+    $NetAdapterList = Get-NetAdapter -Physical -InterfaceDescription "$($NetAdapterIfName.("#text"))" -ErrorAction SilentlyContinue
+
+    Switch($true){
+        ($($NetAdapterList | Measure).count -eq 0){Write-Error "`nNo matches found" -Category InvalidResult;exit}
+        ($($NetAdapterList | Measure).count -gt 1){Write-Error "`n Multiple matches found, unable to continue." -Category InvalidResult;exit}
+        ($($NetAdapterList | Measure).count -eq 1){
+            Write-Host -ForegroundColor Green "Match found"
+            Write-host -ForegroundColor Yellow -NoNewline "Checking the driver version specified in XML File... "
+
+            if($NetAdapterList.DriverVersion -eq $NetAdapterDriverVers.("#text")){
+                Write-Host -ForegroundColor Green "Match found"
+                Write-host -ForegroundColor Yellow  "Import settings... "
+
+                Foreach($setting in $NetAdapterAdvSettings){
+                    $GetValue = Select-Xml -Xml $XML -XPath "//Property[contains(@Name,'$($setting.Name)')]//Property[@Name='Value']" | Select -ExpandProperty Node
+        
+                    Write-Host -ForegroundColor Yellow -NoNewline "`t $($setting.Name) : "
+                    if($GetValue.("#text") -ne $null){
+                        Set-NetAdapterAdvancedProperty -InterfaceDescription $NetAdapterList.InterfaceDescription -RegistryKeyword "$($($setting.Name))" -DisplayValue $($GetValue.("#text"))
+                        Write-Host -ForegroundColor green "Applied"
+                        
+                    }else{
+                        Write-Host -ForegroundColor Red "No Value to set"
+                    }
+                }
+                Write-Host -ForegroundColor green "Process Completed"
+            }else{
+                Write-Error "`nDrivers Versions do not match" -Category InvalidResult
+                exit
+            }
+        }
+    }
+}catch{
+    Write-Error "`n $_"
+}
+
+
+
+
+
